2f8f9f04c2d0981ad23faa240524ee3d152e0f04
BharatGuturi <bharatguturi@gmail.com>
2022-09-29 20:20:21 +0800

    -final files

---
+++ Table_creation_scripts/1_Project_2.sql
@@ -0,0 +1,13 @@
+-- Database: Project_2
+
+-- DROP DATABASE IF EXISTS "Project_2";
+
+CREATE DATABASE "Project_2"
+    WITH
+    OWNER = postgres
+    ENCODING = 'UTF8'
+    LC_COLLATE = 'English_Australia.1252'
+    LC_CTYPE = 'English_Australia.1252'
+    TABLESPACE = pg_default
+    CONNECTION LIMIT = -1
+    IS_TEMPLATE = False;
